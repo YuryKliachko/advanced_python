fcbc8b41e925ce9b0078778831e2fb13a9680162
Yury Kliachko <yuru_kliachko@epam.com>
2019-02-28 06:51:25 +0300

    homework 10

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='Homework_9',
+    version='0.1',
+    description='Homework 9 for Advanced Python course',
+    author='Yury Kliachko',
+    author_email='yury_kliachko@epam.com',
+    url='http://www.epam.com',
+    packages=['homework9']
+)
